52d5d9e135fe03be5c471119ae00b080577df0d8
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2022-04-01 00:43:27 +0200

    Update Igrica1Form.Designer.cs

--- Igrica1Form.Designer.cs
+++ Igrica1Form.Designer.cs
@@ -80,10 +80,10 @@ namespace Hackathon_Project_GUI
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Red;
-            this.pictureBox2.Location = new System.Drawing.Point(-5, 397);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(1, 356);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(294, 199);
+            this.pictureBox2.Size = new System.Drawing.Size(305, 240);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.progres3);
@@ -92,10 +92,10 @@ namespace Hackathon_Project_GUI
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox3.Location = new System.Drawing.Point(322, 397);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(306, 356);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(279, 199);
+            this.pictureBox3.Size = new System.Drawing.Size(305, 240);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.DragLeave += new System.EventHandler(this.progres2);
@@ -103,10 +103,10 @@ namespace Hackathon_Project_GUI
             // pictureBox4
             // 
             this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.pictureBox4.Location = new System.Drawing.Point(642, 397);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox4.Location = new System.Drawing.Point(605, 356);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(279, 199);
+            this.pictureBox4.Size = new System.Drawing.Size(305, 240);
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             this.pictureBox4.DragOver += new System.Windows.Forms.DragEventHandler(this.progres3);
@@ -260,7 +260,7 @@ namespace Hackathon_Project_GUI
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(913, 596);
+            this.ClientSize = new System.Drawing.Size(929, 635);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
