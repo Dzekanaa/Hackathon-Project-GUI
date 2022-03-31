e5e20243a8f7ba10d066c2490a779863eb67a29e
Ika1009 <ilija.doncic@yahoo.com>
2022-03-31 17:07:32 +0200

    dodao dugme u settings

--- settingsForm.Designer.cs
+++ settingsForm.Designer.cs
@@ -29,25 +29,25 @@ namespace Hackathon_Project_GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.ugasiMuzikuButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // ugasiMuzikuButton
             // 
-            this.button1.Location = new System.Drawing.Point(58, 59);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ugasiMuzikuButton.Location = new System.Drawing.Point(113, 119);
+            this.ugasiMuzikuButton.Name = "ugasiMuzikuButton";
+            this.ugasiMuzikuButton.Size = new System.Drawing.Size(106, 41);
+            this.ugasiMuzikuButton.TabIndex = 0;
+            this.ugasiMuzikuButton.Text = "ugasi muziku";
+            this.ugasiMuzikuButton.UseVisualStyleBackColor = true;
+            this.ugasiMuzikuButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // settingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(363, 333);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ugasiMuzikuButton);
             this.Name = "settingsForm";
             this.Text = "settingsForm";
             this.ResumeLayout(false);
@@ -56,6 +56,6 @@ namespace Hackathon_Project_GUI
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ugasiMuzikuButton;
     }
 }
--- settingsForm.cs
+++ settingsForm.cs
@@ -19,7 +19,7 @@ namespace Hackathon_Project_GUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
